265f68ff5c8a79eb5ec1d00921fc53e54e440727
reyhannayaka <92003134+reyhannayaka@users.noreply.github.com>
2021-12-17 09:30:30 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "PHP02";
+
+    $conn = new mysqli($host, $user, $pass, $db);
+
+    //  if($conn){
+    //    echo "koneksi berhasil";
+    //  }else{
+    //    echo "koleksi gagal";
+    //  }
+?>
